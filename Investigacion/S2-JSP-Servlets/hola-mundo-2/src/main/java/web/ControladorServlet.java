8b3f37c0a38cadd48189bebb250b19460b53d872
Andres Vargas <a.m.vargas1996@gmail.com>
2022-04-21 22:33:50 -0300

    Sesion 2: JSP, Servlets

---
+++ Investigacion/S2-JSP-Servlets/hola-mundo-2/src/main/java/web/ControladorServlet.java
@@ -0,0 +1,46 @@
+package web;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ControladorServlet
+ */
+public class ControladorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControladorServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Guarda la ruta del JSP encargado de la vista en un string
+		String jspEncargadoVista = "/WEB-INF/jsp/view/vista.jsp";
+		
+		// Hace que el jsp que se encarga de la vista se encargue de generar el documento html
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(jspEncargadoVista);
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
